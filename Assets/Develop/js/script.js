d2ed683969ca8d585998a1a5f44536b77beddb81
nbwong <nicholas.b.wong@gmail.com>
2022-07-19 23:53:41 -0700

    updated read me file and most of the script file.

--- Assets/Develop/js/script.js
+++ Assets/Develop/js/script.js
@@ -7,6 +7,25 @@ $("#currentDay").text(todayEl.format("dddd, MMMM Do"));
 console.log(todayEl);
 var saveBtn = $(".saveBtn");
 
-$( ".saveBtn" ).button({
-    icon: "ui-icon-lock"
-  });
+function timeBlockColor() {
+    var hour = moment().hours();
+
+    $(".time-block").each(function() {
+        var currentHour = parseInt($(this).attr("id"));
+
+        if (currentHour > hour) {
+            $(this).addClass("future");
+        } else if (currentHour === hour) {
+            $(this).addClass("present")
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
+
+saveBtn.on("click", function() {
+    var time = $(this).siblings(".hour").text;
+    var plan = $(this).siblings(".plan").val();
+
+    localStorage.setItem(time, plan);
+});
