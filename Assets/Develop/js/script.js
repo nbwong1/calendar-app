5ed4af3413ee73d0cd4659626db4cc9a2f718a7e
nbwong <nicholas.b.wong@gmail.com>
2022-07-19 21:23:35 -0700

    Added timeblocks to my HTML file

--- Assets/Develop/js/script.js
+++ Assets/Develop/js/script.js
@@ -1,2 +1,12 @@
-var dayEl = moment();
-$("#currentDay").text(dayEl.format("dddd, MMMM Do"));
+// added date at the top of the page
+
+var todayEl = moment();
+$("#currentDay").text(todayEl.format("dddd, MMMM Do"));
+
+//console log the date for today
+console.log(todayEl);
+
+var rootEl = $('#root');
+
+var nineAmEl = $('card');
+nineAmEl.attr('id', '#nineAmEl');
